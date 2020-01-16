261f3026e540154a7efd2fd433b84256d9d44037
srikanth kalyan <kalyan.rachuru@gmail.com>
2020-01-16 15:21:21 +0530

    git jenkins

--- https/github.com/kalyanrachuru/GIT4.git/kal.com/src/test/java/kal/com/GitJenkinsTest.java
+++ https/github.com/kalyanrachuru/GIT4.git/kal.com/src/test/java/kal/com/GitJenkinsTest.java
@@ -1,5 +1,12 @@
 package kal.com;
 
+import org.testng.annotations.Test;
+
 public class GitJenkinsTest {
 
+	@Test
+	public void display() {
+		
+		System.out.println("testing git and jenkins ");
+	}
 }
